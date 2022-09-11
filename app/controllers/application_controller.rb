a2d834e28689d76de0206d23557c9889b6b60b9a
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2022-09-11 13:11:48 +0000

    [Update]顧客側ログイン、ログアウト後に遷移するpathを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!
+  
+  def after_sign_in_path_for(resource)
+    public_customer_path(@customer.id) # ログイン後に遷移するpathを設定
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
 end
