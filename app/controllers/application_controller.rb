98f9bf780e24a1d0cedf6104d29f182e92239bfc
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2022-09-15 13:58:19 +0000

    [Update]applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!
+  before_action :authenticate_customer!, except: [:about]
   
   def after_sign_in_path_for(resource)
     public_customer_path(@customer.id) # ログイン後に遷移するpathを設定
