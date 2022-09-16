1111211672dc6172939071ef7eb102b5a5981539
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2022-09-16 13:34:14 +0000

    [Update]ActionController(before_action追加)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:about]
+  before_action :configure_permitted_parameters , if: :devise_controller?
   
   def after_sign_in_path_for(resource)
     public_customer_path(@customer.id) # ログイン後に遷移するpathを設定
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::Base
   
   def after_sign_out_path_for(resource)
     root_path # ログアウト後に遷移するpathを設定
+  end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :address, :phone_number])
   end
 end
