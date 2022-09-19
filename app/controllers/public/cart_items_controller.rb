038c0824e9465cb21f72be620c891aae8c6981f2
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2022-09-19 02:04:54 +0000

    [Update]public/items/show

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,24 @@
 class Public::CartItemsController < ApplicationController
   def index
+     @cart_items = current_customer.cart_items
+     
   end
 
   def create
   end
 
   def update
+  end
+  
+  def destroy
+  end
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,5 +5,12 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active )
   end
 end
