92dc6ca13063370c9fcde08f57d4d178e6a154e6
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2022-10-02 02:12:15 +0000

    [Updata]public/orders/show(注文内容表示編集中)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,9 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
   
+  def subtotal# 小計を求めるメソッド
+    item.with_tax_price * amount
+  end
+  
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
