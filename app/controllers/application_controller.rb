a5d5f09c1e619c30908b885d031018efeb0adb62
EC2 Default User <ec2-user@ip-172-31-41-67.ap-northeast-1.compute.internal>
2022-10-16 06:19:24 +0000

    [Update]orderに時間の表示を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters , if: :devise_controller?
   
-  def after_sign_in_path_for(resource_or_scope)#ログイン後に遷移するpathを設定
-    if resource_or_scope.is_a?(Admin)
-      admin_orders_path
-    else
-      root_path
-    end
-  end
-  
   
   def move_to_signed_in
     unless customer_signed_in?
